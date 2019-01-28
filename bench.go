344122555bbf3208845f1323d874d5853a012ed7
Andres Orozco <ao15@my.fsu.edu>
2019-01-28 14:27:36 -0500

    fix channel block

--- bench.go
+++ bench.go
@@ -4,13 +4,14 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
 type Bench struct {
 	testers map[string]*LoadTester
 	ch      chan *Stats
-	conf    config
+	stats   map[string]*Stats
 }
 
 func NewBench(path string) (*Bench, error) {
@@ -18,6 +19,7 @@ func NewBench(path string) (*Bench, error) {
 	b := &Bench{
 		testers: make(map[string]*LoadTester),
 		ch:      make(chan *Stats),
+		stats:   make(map[string]*Stats),
 	}
 
 	conf, err := fromJSON(path)
@@ -46,14 +48,28 @@ func NewBench(path string) (*Bench, error) {
 	return b, nil
 }
 
+// Run a benchmark test with given config
 func (b *Bench) Run() {
+	var wg sync.WaitGroup
+
+	go b.handleStats()
+
 	for _, tester := range b.testers {
-		tester.Run(b.ch)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			tester.Run(b.ch)
+		}()
 	}
+	wg.Wait()
 
 }
 
 func (b *Bench) handleStats() {
 	// read in statistics
+
+	for stat := range b.ch {
+		b.stats[stat.Endpoint] = stat
+	}
 
 }
